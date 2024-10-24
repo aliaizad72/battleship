bc5db0dd088b43173968b4b18e7eb2fedd0fe8f7
Ali Aizad <aliaizad72@gmail.com>
2024-10-24 19:26:18 +0800

    Relatively position ships

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import "./global.css";
 import Game from "./game";
 
 const game = new Game();
-game.players.forEach((player) => {
-  createBoard(player);
-  // addShipsToBoard(player);
-});
+const player = game.players[0];
+createBoard(player);
+createShip(player);
 
 //create gameboard element and assign an id of player
 function createBoard(player) {
+  const gridContainer = document.createElement("div");
+  gridContainer.className = "w-[450px] mx-auto my-4 relative";
   const grid = document.createElement("div");
   grid.id = player.name;
-  grid.className = "w-[450px] grid grid-cols-10 grid-rows-10 mx-auto my-4";
+  grid.className = "grid grid-cols-10 grid-rows-10";
 
   for (let i = 0; i < 100; i++) {
     const square = document.createElement("div");
@@ -28,7 +29,8 @@ function createBoard(player) {
 
     grid.appendChild(square);
   }
-  document.body.appendChild(grid);
+  gridContainer.appendChild(grid);
+  document.body.appendChild(gridContainer);
 }
 
 function strCoords(num) {
@@ -39,20 +41,32 @@ function strCoords(num) {
   return num;
 }
 
-function addShipsToBoard(player) {
-  player.gameboard.ships.forEach((ship) => {
-    for (let i = 0; i < ship.coords.length; i++) {
-      const [x, y] = ship.coords[i];
-      const shipDiv = square(player, x, y);
-      // shipDiv.classList.remove("border-r");
-      // shipDiv.classList.add("bg-blue-50");
-      console.log(shipDiv.parentElement);
-      console.log(shipDiv);
-    }
-  });
+function createShip(player) {
+  const shipDiv = document.createElement("div");
+  shipDiv.className =
+    "w-1/2 border-2 bg-gray-50 border-black h-10 absolute top-10 hover:cursor-move";
+  shipDiv.draggable = true;
+  document.getElementById(player.name).parentElement.appendChild(shipDiv);
 }
 
+// function addShipsToBoard(player) {
+//   player.gameboard.ships.forEach((ship) => {
+//     for (let i = 0; i < ship.coords.length; i++) {
+//       const [x, y] = ship.coords[i];
+//       const shipDiv = square(player, x, y);
+//       if (ship.direction === "horizontal") {
+//         shipDiv.classList.add("bg-blue-100", "border-2");
+//         if (i != ship.coords.length - 1) {
+//           shipDiv.classList.add("border-r-blue-100");
+//         }
+//       }
+//     }
+//   });
+// }
+
 function square(player, x, y) {
-  const boardArr = [...document.getElementById(player.name).children];
-  return [...boardArr[x].children][y];
+  const boardArr = document.getElementById(player.name).children;
+  const index = Number(x.toString() + y.toString());
+  console.log(index);
+  return boardArr[index];
 }
