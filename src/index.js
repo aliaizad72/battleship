54b642054a29b932033d2cf3761c6a5c4b8d2f47
Ali Aizad <aliaizad72@gmail.com>
2024-10-26 02:56:59 +0800

    Reorder and add comments

--- src/index.js
+++ src/index.js
@@ -11,141 +11,153 @@ addShipsToBoard(player);
 //create gameboard element and assign an id of player
 // w and h of each grid squares = 45px
 function createBoard(player) {
-  const gridContainer = document.createElement("div");
-  gridContainer.className = `mx-auto relative grid-container`;
-  gridContainer.style.width = `${gridW}px`;
+	const gridContainer = document.createElement("div");
+	gridContainer.className = `mx-auto relative grid-container`;
+	gridContainer.style.width = `${gridW}px`;
+	gridContainer.addEventListener("dragover", e => e.preventDefault());
+	gridContainer.addEventListener("drop", e => gridDrop(e));
 
-  const grid = document.createElement("div");
-  grid.id = player.name;
-  grid.className = "grid";
-  grid.style.gridTemplateColumns = `repeat(10, ${squareW}px)`;
-  grid.style.gridTemplateRows = `repeat(10, ${squareW}px)`;
+	const grid = document.createElement("div");
+	grid.id = player.name;
+	grid.className = "grid";
+	grid.style.gridTemplateColumns = `repeat(10, ${squareW}px)`;
+	grid.style.gridTemplateRows = `repeat(10, ${squareW}px)`;
 
-  for (let i = 0; i < 100; i++) {
-    const square = document.createElement("div");
-    square.dataset.coords = `${strCoords(i)}`.split("");
-    square.className = `border-2 bg-blue-100 border-blue-300 square`;
+	for (let i = 0; i < 100; i++) {
+		const square = document.createElement("div");
+		square.dataset.coords = `${strCoords(i)}`.split("");
+		square.className = `border-2 bg-blue-100 border-blue-300 square`;
 
-    if (i % 10 !== 0) {
-      square.classList.add("border-l-0");
-    }
+		if (i % 10 !== 0) {
+			square.classList.add("border-l-0");
+		}
 
-    if (100 - i > 10) {
-      square.classList.add("border-b-0");
-    }
+		if (100 - i > 10) {
+			square.classList.add("border-b-0");
+		}
 
-    grid.appendChild(square);
-  }
-  gridContainer.appendChild(grid);
-  document.body.appendChild(gridContainer);
+		grid.appendChild(square);
+	}
+
+	gridContainer.appendChild(grid);
+	document.body.appendChild(gridContainer);
 }
 
-function addShipsToBoard(player) {
-  player.gameboard.ships.forEach((obj, i) => {
-    createShip(obj, i);
-  });
+function gridDrop(e) {
+	e.preventDefault();
+	//find the beginning and end square to find the distance
+	const startSquare = e.dataTransfer.getData("startSquare");
+	const endSquare = getSquareFromDrag(e);
+	const d = distance(startSquare, endSquare);
+
+	//get ship element that was dragged
+	const ship = document.getElementById(e.dataTransfer.getData("draggable"));
+
+	//find the coords of left and right bound of grid
+	const leftBound = document
+		.querySelector(".grid-container")
+		.getBoundingClientRect().left;
+	const rightBound = document
+		.querySelector(".grid-container")
+		.getBoundingClientRect().right;
+
+	//in case move invalid, get coordinates to translate back to earlier pos
+	const beforeTranslate = getCoords(ship);
+
+	translateShip(ship, d);
+
+	// find abs position of ship after translation
+	const shipAbsPos = ship.getBoundingClientRect();
+
+	// move back ship to original position if move invalid
+	if (
+		shipAbsPos.left < leftBound ||
+		// +1 to offset weird pixelation of ships length 3
+		shipAbsPos.right > rightBound + 1 ||
+		isOverlapped(ship)
+	) {
+		ship.style.transform = `translate(${beforeTranslate.x}px, ${beforeTranslate.y}px)`;
+	}
 }
 
-function strCoords(num) {
-  if (num < 10) {
-    return "0" + num;
-  }
-
-  return num;
+//this function returns the square element where the pointer was when drag event ends
+function getSquareFromDrag(e) {
+	return document
+		.elementsFromPoint(e.clientX, e.clientY)
+		.find(e => e.classList.contains("square")).dataset.coords;
 }
 
-function createShip(obj, i) {
-  const shipDiv = document.createElement("div");
-  const [r, c] = obj.coords[0];
-  shipDiv.className = `bg-blue-950 border border-blue-300 absolute hover:cursor-move ship`;
-  shipDiv.style.height = `${squareW}px`;
-  shipDiv.style.width = `${obj.ship.length * squareW}px`;
-  shipDiv.style.left = `${c * squareW}px`;
-  shipDiv.style.top = `${r * squareW}px`;
-  shipDiv.draggable = true;
-  shipDiv.id = `${player.name}-ship-${i}`;
-  shipDiv.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("draggable", event.target.id);
-    event.dataTransfer.setData("startSquare", getSquareFromDrag(event));
-  });
-  document.getElementById(player.name).parentElement.appendChild(shipDiv);
-}
-
-document
-  .querySelector(".grid-container")
-  .addEventListener("dragover", (event) => {
-    event.preventDefault();
-  });
-
-document.querySelector(".grid-container").addEventListener("drop", (event) => {
-  event.preventDefault();
-  const startSquare = event.dataTransfer.getData("startSquare");
-  const endSquare = getSquareFromDrag(event);
-  const squaresMoved = moveVector(startSquare, endSquare);
-  const ship = document.getElementById(event.dataTransfer.getData("draggable"));
-  const leftBound = document
-    .querySelector(".grid-container")
-    .getBoundingClientRect().left;
-  const rightBound = document
-    .querySelector(".grid-container")
-    .getBoundingClientRect().right;
-  const beforeTranslatePos = getTranslateVals(ship);
-  translateShip(ship, squaresMoved);
-  const shipAbsPos = ship.getBoundingClientRect();
-  // +1 to offset weird pixelation of ships length 3
-  if (
-    shipAbsPos.left < leftBound ||
-    shipAbsPos.right > rightBound + 1 ||
-    isOverlapped(ship)
-  ) {
-    ship.style.transform = `translate(${beforeTranslatePos.x}px, ${beforeTranslatePos.y}px)`;
-  }
-});
-
-function isOverlapped(ship) {
-  const rect = ship.getBoundingClientRect();
-  const leftTop =
-    document
-      .elementsFromPoint(rect.left + 1, rect.top)
-      .filter((e) => e.classList.contains("ship")).length === 2;
-  const rightTop =
-    document
-      .elementsFromPoint(rect.right - 1, rect.top)
-      .filter((e) => e.classList.contains("ship")).length === 2;
-  // const rightBot = document.elementsFromPoint(rect.right - 1, rect.bottom - 1);
-  // const leftBot = document.elementsFromPoint(rect.left, rect.bottom - 1);
-  return leftTop || rightTop;
-}
-
-function getTranslateVals(element) {
-  const style = window.getComputedStyle(element);
-  const matrix = new DOMMatrixReadOnly(style.transform);
-  return {
-    x: matrix.m41,
-    y: matrix.m42,
-  };
-}
-
-function getSquareFromDrag(event) {
-  return document
-    .elementsFromPoint(event.clientX, event.clientY)
-    .find((e) => e.classList.contains("square")).dataset.coords;
+function distance(startSquare, endSquare) {
+	const [r1, c1] = strToCoords(startSquare);
+	const [r2, c2] = strToCoords(endSquare);
+	return [r2 - r1, c2 - c1];
 }
 
 function strToCoords(str) {
-  return str.split(",").map((coord) => Number(coord));
+	return str.split(",").map(coord => Number(coord));
 }
 
-function moveVector(startSquare, endSquare) {
-  const [r1, c1] = strToCoords(startSquare);
-  const [r2, c2] = strToCoords(endSquare);
-  return [r2 - r1, c2 - c1];
+function getCoords(element) {
+	const style = window.getComputedStyle(element);
+	const matrix = new DOMMatrixReadOnly(style.transform);
+	return {
+		x: matrix.m41,
+		y: matrix.m42,
+	};
 }
 
-function translateShip(ship, moveVector) {
-  const shipPos = getTranslateVals(ship);
-  const [dr, dc] = moveVector;
-  ship.style.transform = `translate(${shipPos.x + dc * squareW}px, ${
-    shipPos.y + dr * squareW
-  }px)`;
+function translateShip(ship, distance) {
+	const shipPos = getCoords(ship);
+	const [dr, dc] = distance;
+	ship.style.transform = `translate(${shipPos.x + dc * squareW}px, ${
+		shipPos.y + dr * squareW
+	}px)`;
+}
+
+function isOverlapped(ship) {
+	const rect = ship.getBoundingClientRect();
+	const leftTop =
+		document
+			.elementsFromPoint(rect.left + 1, rect.top)
+			.filter(e => e.classList.contains("ship")).length === 2;
+	const rightTop =
+		document
+			.elementsFromPoint(rect.right - 1, rect.top)
+			.filter(e => e.classList.contains("ship")).length === 2;
+	// const rightBot = document.elementsFromPoint(rect.right - 1, rect.bottom - 1);
+	// const leftBot = document.elementsFromPoint(rect.left, rect.bottom - 1);
+	return leftTop || rightTop;
+}
+
+function strCoords(num) {
+	if (num < 10) {
+		return "0" + num;
+	}
+	return num;
+}
+
+function addShipsToBoard(player) {
+	player.gameboard.ships.forEach((obj, i) => {
+		createShip(obj, i);
+	});
+}
+
+function createShip(obj, i) {
+	const shipDiv = document.createElement("div");
+	const [r, c] = obj.coords[0];
+
+	shipDiv.className = `bg-blue-950 border border-blue-300 absolute hover:cursor-move ship`;
+	shipDiv.style.height = `${squareW}px`;
+	shipDiv.style.width = `${obj.ship.length * squareW}px`;
+	shipDiv.style.left = `${c * squareW}px`;
+	shipDiv.style.top = `${r * squareW}px`;
+	shipDiv.id = `${player.name}-ship-${i}`;
+
+	shipDiv.draggable = true;
+	shipDiv.addEventListener("dragstart", e => {
+		e.dataTransfer.setData("draggable", e.target.id);
+		e.dataTransfer.setData("startSquare", getSquareFromDrag(e));
+	});
+
+	document.getElementById(player.name).parentElement.appendChild(shipDiv);
 }
